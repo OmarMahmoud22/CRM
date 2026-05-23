e67c61a530bcc89b4ca515d66490ce4828efcbe4
OmarMahmou22 <omahmoudali9@gmail.com>
2026-05-23 12:03:38 +0300

    dash

--- BACKEND/Routes/DashbourdRouter.js
+++ BACKEND/Routes/DashbourdRouter.js
@@ -3,6 +3,6 @@ const router = express.Router()
 
 const {InformationLead} =require('../Dashbord/DashbourdController')
 const {AgentStatus} = require('../Middleware/LeadMidllware')
-router.get('/Dashbourd' ,AgentStatus,InformationLead )
+router.get('/dashboard', AgentStatus, InformationLead)
 
 module.exports = router
