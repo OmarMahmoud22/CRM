26cd88bf9f40736c933eaae72be62604933708f3
OmarMahmou22 <omahmoudali9@gmail.com>
2026-06-01 19:20:08 +0300

    updated

--- FRONTEND/vite-project/src/Compnent/Dash/Dashbourd.jsx
+++ FRONTEND/vite-project/src/Compnent/Dash/Dashbourd.jsx
@@ -1,5 +1,43 @@
-// import React from 'react'
+import axios from "axios";
+import { useEffect, useState } from "react";
 
-export default function Dashbourd() {
-  return <div>Dashbourd</div>;
+export default function Dashboard() {
+  const [stats, setStats] = useState({});
+
+  useEffect(() => {
+    const GetStats = async () => {
+      try {
+        const token = localStorage.getItem("token");
+
+        const { data } = await axios.get(
+          "http://localhost:3000/api/dashboard",
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+        console.log(data);
+        
+
+        setStats(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    GetStats();
+  }, []);
+
+  return (
+    <div>
+      <h1>Total Leads: {stats.totalLead}</h1>
+
+      <h1>New Leads: {stats.New}</h1>
+
+      <h1>Won Leads: {stats.Won}</h1>
+
+      <h1>Lost Leads: {stats.Lost}</h1>
+    </div>
+  );
 }
